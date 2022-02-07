16c80d0bfff8c1f3d4c195b9bed7df49aea943d1
Aadm Klonowski <adamklonowski91@gmail.com>
2022-02-07 12:22:10 +0100

    Adding tests checking the lenght of password.

--- src/main/java/com/example/junittdd/PasswordValidator.java
+++ src/main/java/com/example/junittdd/PasswordValidator.java
@@ -3,9 +3,12 @@ package com.example.junittdd;
 public class PasswordValidator {
 
     public static final int MIN_PASSWORD_LENGHT = 6;
+    public static final int MAX_PASSWORD_LENGHT = 20;
 
     public boolean validate(String password) {
-       return password.length() >= MIN_PASSWORD_LENGHT;
+       return password.length() <= MAX_PASSWORD_LENGHT &&
+              password.length() >= MIN_PASSWORD_LENGHT;
 
     }
 }
+
--- src/test/java/com/example/junittdd/PasswordValidatorTest.java
+++ src/test/java/com/example/junittdd/PasswordValidatorTest.java
@@ -9,6 +9,7 @@ class PasswordValidatorTest {
 
     @Test
     void shouldAcceptPasswordLongerThanSixChars() {
+
         String passwordToCheck = "1234567";
 
         boolean result = passwordValidator.validate(passwordToCheck);
@@ -24,6 +25,27 @@ class PasswordValidatorTest {
         boolean result = passwordValidator.validate(passwordToCheck);
 
         Assertions.assertThat(result).isFalse();
+
+    }
+
+    @Test
+    void shouldNotAcceptPasswordLongerThan20Chars() {
+
+        String passwordLongerThan20Chars = "PasswordLongerThan20Chars";
+
+        boolean result = passwordValidator.validate(passwordLongerThan20Chars);
+
+        Assertions.assertThat(result).isFalse();
+
+    }
+    @Test
+    void shouldNotAcceptPasswordEqual20Chars() {
+
+        String passwordEqual20Chars = "12345678901234567890";
+
+        boolean result = passwordValidator.validate(passwordEqual20Chars);
+
+        Assertions.assertThat(result).isTrue();
 
     }
 }
